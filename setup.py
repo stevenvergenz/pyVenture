5c4b9fdb8110befc1791e0024a88949ccc9426aa
Steven Vergenz <vergenzs@gmail.com>
2012-10-17 11:34:06 -0400

    Reorganized into package format

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = 'pySOG',
+	version = '0.1',
+	packages = find_packages(),
+	entry_points = {
+		'setuptools.installation': [
+			'eggsecutable = pySOG.main:main'
+		]
+	}
+)
+
+
