3f359a9c220c169a90c3dee7d96a57603f63f9f6
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2013-01-03 14:01:30 -0500

    Started work on a main menu map

--- pyVenture/game/main.py
+++ pyVenture/game/main.py
@@ -1,12 +1,36 @@
-from common.types import World, Player, Area, Feature, Action
-from common.events import PlayerMoveEvent
-import json
+from common import types
+from common import events
+import json, gzip, glob
+
+world = None
+player = None
+
+class LoadWorldEvent(events.Event):
+
+	def __init__(self, properties = None):
+		if properties is None:
+			Event.__init__(self, {'filename': ''})
+		else:
+			Event.__init__(self, properties)
+
+
+	def __call__(self, actor, action):
+		dump = {}
+		with gzip.open(filename, 'r') if filename[-2:] == 'gz' else open(filename, 'r') as file:
+			try:
+				tempstr = file.read()
+				dump = json.loads(tempstr)
+			except:
+				print 'There was a problem loading', filename
+
+		# populate tree
+		world = types.World.deserialize(dump)
+		player = world.player
+
 
 def buildWorld():
 
-	dump = json.loads('{"player": {"inventory": [], "name": "Hero", "currentArea": "Courtyard 1"}, "areas": {"Library 1": {"entranceText": "You arrive in a quiet musty-smelling room with rows and rows of bookshelves.", "name": "Library", "features": [{"name": "eastern door", "actions": [{"description": "Enter eastern door", "events": [{"text": "You traverse the passage.", "type": "TextEvent"}, {"destination": "Courtyard 1", "type": "PlayerMoveEvent"}]}], "description": "Wooden double-doors to the east"}]}, "Courtyard 1": {"entranceText": "You arrive in a brightly lit dirt practice yard with several dummies set up.", "name": "Courtyard", "features": [{"name": "western stairway", "actions": [{"description": "Enter western stairway", "events": [{"text": "You traverse the passage.", "type": "TextEvent"}, {"destination": "Library 1", "type": "PlayerMoveEvent"}]}], "description": "A winding staircase to the west leading to a stone tower"}, {"name": "southern door", "actions": [{"description": "Enter southern door", "events": [{"text": "You traverse thepassage.", "type": "TextEvent"}, {"destination": "Armory 1", "type": "PlayerMoveEvent"}]}], "description": "A sturdy-looking wooden door to the south"}]}, "Birth Springs 1": {"entranceText": "You awake in a damp dark stone room.", "name": "Birth Springs", "features": [{"name": "dark tunnel", "actions": [{"description": "Enter dark tunnel", "events": [{"text": "You traverse the passage.", "type": "TextEvent"}, {"destination": "Courtyard 1", "type": "PlayerMoveEvent"}]}], "description": "A long dark tunnel to the south with a light at the end"}]}, "Armory 1": {"entranceText": "You arrive in a cramped room full of weapon and armor racks.", "name": "Armory", "features": [{"name": "northern door", "actions": [{"description": "Enter northern door", "events": [{"text": "You traverse the passage.", "type": "TextEvent"}, {"destination": "Courtyard 1", "type": "PlayerMoveEvent"}]}], "description": "A sturdy wooden door to the north"}]}}}')
-	world = World.deserialize(dump)
-	return world
+	pass
 
 
 def main():
@@ -40,6 +64,7 @@ def main():
 
 	print json.dumps( world.serialize() )
 	raw_input('Press Enter to quit...')
+
 
 
 if __name__ == '__main__':
